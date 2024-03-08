ba44a2d284a05c69a5f86d44f79335c95ce00432
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-08 10:57:26 +0100

    Add code to save button

--- SaveText/frmSaveText.cs
+++ SaveText/frmSaveText.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,12 @@ namespace SaveText
         {
             // waar en onder welke naam moet het bestand
             // bewaard worden
+            string path = @"C:\verhalen\" + txtTitle.Text + ".txt";
+
+            // inhoud van grote textbox in file 'schrijven'
+            // path = waar moet het bestand bewaard worden
+            // txtStory.Text --> de tekst die in het bestand moet komen
+            File.WriteAllText(path, txtStory.Text);
         }
     }
 }
