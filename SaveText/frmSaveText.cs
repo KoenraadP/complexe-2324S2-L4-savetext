e84e84f06dda57f8c9dcb396a9c9f92b026a589e
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-03-08 10:51:28 +0100

    Add project files.

---
+++ SaveText/frmSaveText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaveText
+{
+    public partial class frmSaveText : Form
+    {
+        public frmSaveText()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // waar en onder welke naam moet het bestand
+            // bewaard worden
+        }
+    }
+}
